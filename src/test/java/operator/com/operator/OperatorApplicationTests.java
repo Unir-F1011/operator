a837295a4c524f98696d663a2c91f5ced824570c
MBI-88 <ingmbi8807@gmail.com>
2025-06-16 08:10:16 -0300

    layout done

---
+++ src/test/java/operator/com/operator/OperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package operator.com.operator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
