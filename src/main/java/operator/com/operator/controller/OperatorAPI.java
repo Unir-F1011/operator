92a31aa264385881de2c412bed5ef62026e96183
MBI-88 <ingmbi8807@gmail.com>
2025-06-27 07:33:17 -0300

    fixed bugg in controller

--- src/main/java/operator/com/operator/controller/OperatorAPI.java
+++ src/main/java/operator/com/operator/controller/OperatorAPI.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
@@ -22,7 +21,6 @@ import operator.com.operator.service.InnerOperator;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/operator")
 @Slf4j
 public class OperatorAPI {
 
